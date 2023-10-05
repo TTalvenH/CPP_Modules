6c2954bf2eebb4c2c6386b94b2cfface83620e6d
Tuomo Talvenheimo <tuomo.talvenheimo@outlook.com>
2023-10-05 12:10:37 +0300

    module01/ex04 done

--- module01/ex04/src/main.cpp
+++ module01/ex04/src/main.cpp
@@ -3,7 +3,37 @@
 #include <fstream>
 
 
-int	main(int argc, char *argv[])
-{
-	
+int	main(int argc, char *argv[]){
+	if (argc == 4){
+		std::string		line;
+		std::string		fileName = argv[1];
+		std::string		oldWord = argv[2];
+		std::string		newWord = argv[3];
+
+		std::fstream	file(fileName, std::ios::in | std::ios::out);
+		if (!file.is_open()){
+			std::cerr << "Error opening input file!" << std::endl;
+			return 1;
+		}
+		std::fstream	fileOut(fileName + ".replace", std::ios::out);
+		if (!fileOut.is_open()){
+			std::cerr << "Error opening output file!" << std::endl;
+			file.close(); fileOut.close();
+			return 1;
+		}	
+		while (std::getline(file, line)){
+			size_t		pos = 0;
+			while (oldWord.length() && (pos = line.find(oldWord, pos)) != std::string::npos){
+			line.erase(pos, oldWord.length());
+			line.insert(pos, newWord);
+			pos += newWord.length();
+			}
+			fileOut << line;
+		}
+		file.close();
+		fileOut.close();
+	}
+	else
+		std::cerr << "Error wrong amount of arguments" << std::endl;
+	return 0;
 }
