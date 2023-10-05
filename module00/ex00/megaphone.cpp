d82bddeb466aa2f892ffdb456baae10d2f8e5218
Tuomo Talvenheimo <tuomo.talvenheimo@outlook.com>
2023-10-05 14:12:04 +0300

    small mod00/ex00 edit

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -2,26 +2,20 @@
 #include <cctype>
 #include <cstring>
 
-void	StringToUpper(char* str)
-{
-	for(size_t i = 0; str[i] != '\0'; i++)
-	{
+void	StringToUpper(char* str){
+	for(size_t i = 0; str[i] != '\0'; i++){
 		str[i] = std::toupper(static_cast<unsigned char>(str[i]));
 	}
 }
 
-int	main(int argc, char **argv)
-{
-	if (argc == 1)
-	{
+int	main(int argc, char **argv){
+	if (argc == 1){
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
-	for(int i = 1; i < argc; i++)
-	{
+	for(int i = 1; i < argc; i++){
 		StringToUpper(argv[i]);
-		for(size_t j = 0; argv[i][j] != '\0'; j++)
-		{
+		for(size_t j = 0; argv[i][j] != '\0'; j++){
 			std::cout << argv[i][j];
 		}
 	}
