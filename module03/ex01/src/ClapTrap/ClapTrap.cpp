1b68d33a0c2ef1fb1b70066b33511302df0014e3
Tuomo Talvenheimo <tuomo.talvenheimo@outlook.com>
2023-10-07 17:47:42 +0300

    mod03/ex01 done

---
+++ module03/ex01/src/ClapTrap/ClapTrap.cpp
@@ -0,0 +1,63 @@
+#include <ClapTrap.hpp>
+
+ClapTrap::ClapTrap()
+	: m_name("")
+	, m_hp(10)
+	, m_energy(10)
+	, m_dmg(0){
+	std::cout << "Claptrap constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap( const std::string& name )
+	: m_name(name)
+	, m_hp(10)
+	, m_energy(10)
+	, m_dmg(0){
+	std::cout << "Claptrap name Constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap( const ClapTrap& copy )
+	: m_name(copy.m_name)
+	, m_hp(copy.m_hp)
+	, m_energy(copy.m_energy)
+	, m_dmg(copy.m_dmg){
+	std::cout << "Claptrap copy constructor called" << std::endl;
+}
+
+ClapTrap::~ClapTrap(){
+	std::cout << "Claptrap deconstructor called" << std::endl;
+}
+
+ClapTrap&	ClapTrap::operator=( const ClapTrap& other ){
+	std::cout << "Claptrap copy assignment operator called" << std::endl;
+	if (this != &other){
+		m_name = other.m_name;
+		m_hp = other.m_hp;
+		m_energy = other.m_energy;
+		m_dmg = other.m_dmg;
+	}
+	return *this;
+}
+
+void	ClapTrap::attack( const std::string& target){
+	if (m_energy > 0 && m_hp > 0){
+		m_energy--;
+		std::cout << "ClapTrap " << m_name << " attacks " << target << ", causing " << m_dmg << " points of damage!" << std::endl;
+	}
+}
+
+void	ClapTrap::takeDamage( unsigned int amount ){
+	m_hp -= amount;
+	std::cout << "ClapTrap " << m_name << " takes " << amount << " damage!" << std::endl;
+	if (m_hp < 0){
+		m_hp = 0;
+	}
+}
+
+void	ClapTrap::beRepaired( unsigned int amount ){
+	if (m_energy > 0 && m_hp > 0){
+		m_energy--;
+		m_hp += amount;
+		std::cout << "ClapTrap " << m_name << " repairs " << amount << " of Hit Points!" << std::endl;
+	}
+}
---
+++ module03/ex01/src/ScavTrap/ScavTrap.cpp
@@ -0,0 +1,48 @@
+#include <ScavTrap.hpp>
+
+ScavTrap::ScavTrap() : ClapTrap(""){
+	m_hp = 100;
+	m_energy = 50;
+	m_dmg = 20;
+	std::cout << "Scavtrap Constructor called" << std::endl;
+}
+
+ScavTrap::ScavTrap( const std::string& name ) : ClapTrap(name){
+	m_hp = 100;
+	m_energy = 50;
+	m_dmg = 20;
+	std::cout << "Scavtrap name constructor called" << std::endl;
+}
+
+ScavTrap::ScavTrap( const ScavTrap& copy ) : ClapTrap(copy.m_name){
+	m_hp = copy.m_hp;
+	m_energy = copy.m_energy;
+	m_dmg = copy.m_dmg;
+	std::cout << "Scavtrap copy constructor called" << std::endl;
+}
+
+ScavTrap::~ScavTrap(){
+	std::cout << "Scavtrap deconstructor called" << std::endl;
+}
+
+ScavTrap&	ScavTrap::operator=( const ScavTrap& other ){
+	std::cout << "Scavtrap copy assignment operator called" << std::endl;
+	if (this != &other){
+		m_name = other.m_name;
+		m_hp = other.m_hp;
+		m_energy = other.m_energy;
+		m_dmg = other.m_dmg;
+	}
+	return *this;
+}
+
+void	ScavTrap::attack( const std::string& target){
+	if (m_energy > 0 && m_hp > 0){
+		m_energy--;
+		std::cout << "ScavTrap " << m_name << " attacks " << target << ", causing " << m_dmg << " points of damage!" << std::endl;
+	}
+}
+
+void	ScavTrap::guardGate(){
+	std::cout << m_name << " is in Gate keeper mode!" << std::endl;
+}
