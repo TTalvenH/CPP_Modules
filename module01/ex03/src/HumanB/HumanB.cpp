8d0510810c6019ccda80e59830cf55613406a8e3
Tuomo Talvenheimo <tuomo.talvenheimo@outlook.com>
2023-10-11 13:40:07 +0300

    added error check to humanB if attacks without weapon

--- module01/ex03/src/HumanB/HumanB.cpp
+++ module01/ex03/src/HumanB/HumanB.cpp
@@ -5,6 +5,10 @@ HumanB::HumanB( std::string name ) : m_name(name), m_weapon(NULL) {}
 HumanB::~HumanB(){}
 
 void	HumanB::attack(){
+	if (m_weapon == NULL){
+		std::cout << m_name << " doesn't have a weapon!!" << std::endl;
+		return;
+	}
 	std::cout << m_name << " attacks with their " << m_weapon->getType() << std::endl;
 }
 
