392054abbbb260323e11c909554c15f01498a253
Tuomo Talvenheimo <tuomo.talvenheimo@outlook.com>
2023-10-13 12:45:41 +0300

    mod02/ex00 checked and added some tests

--- module02/ex00/src/main.cpp
+++ module02/ex00/src/main.cpp
@@ -3,19 +3,30 @@
 int	main()
 {	
 	Fixed a;
+	std::cout << a.getRawBits() << std::endl;
+	a.setRawBits(10);
+
+	std::cout << "\n========================\n" << std::endl;
+
 	Fixed b( a );
 	Fixed c;
 
-	b.setRawBits(5);
+	std::cout << "\n========================\n" << std::endl;
 
-	c = b;
+	c.setRawBits(20);
+
+	std::cout << "\n========================\n" << std::endl;
 
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits()  << std::endl;
 
+	std::cout << "\n========================\n" << std::endl;
+
 	c = a;
 	std::cout << c.getRawBits()  << std::endl;
+
+	std::cout << "\n========================\n" << std::endl;
 
 	return 0;
 }
