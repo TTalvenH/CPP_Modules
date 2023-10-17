959bfd777662922c15ad6f2d78b522ed5252a4a4
Tuomo Talvenheimo <tuomo.talvenheimo@outlook.com>
2023-10-17 11:40:37 +0300

    added tests mod04/ex00

--- module04/ex00/src/main.cpp
+++ module04/ex00/src/main.cpp
@@ -4,13 +4,56 @@
 
 int main(){
 
+	{
+	std::cout << "\n=========== PDF Tests ================\n" << std::endl;
+
 	const Animal* meta = new Animal();
 	const Animal* j = new Dog();
-	const WrongAnimal* i = new WrongCat();
+	const Animal* i = new Cat();
+
+	std::cout << "\n-------------------------\n" << std::endl;
+
+
 	std::cout << j->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
 	i->makeSound(); //will output the cat sound!
 	j->makeSound();
 	meta->makeSound();
-	return 0;
+
+	std::cout << "\n-------------------------\n" << std::endl;
+	
+	delete(meta);
+	delete(j);
+	delete(i);
+
+	}
+
+	{
+		std::cout << "\n=========== My Tests ================\n" << std::endl;
+
+		Animal* cat = new Cat();
+		Animal* dog = new Dog();
+		WrongAnimal* wrong_cat = new WrongCat();
+
+		Dog puppe;
+		Dog copy_puppe(puppe);
+
+		Cat kitty;
+		Cat copy_kitty(kitty);
+
+
+		std::cout << "\n-------------------------\n" << std::endl;
+
+		cat->makeSound();
+		dog->makeSound();
+		wrong_cat->makeSound();
+
+
+		std::cout << "\n-------------------------\n" << std::endl;
+
+
+		delete(cat);
+		delete(wrong_cat);
+		delete(dog);
+	}
 }
