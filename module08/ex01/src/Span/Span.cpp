45b25b38458d1dcbc230de6044e131b2a6f1ce83
Tuomo Talvenheimo <tuomo.talvenheimo@outlook.com>
2023-10-31 09:13:34 +0200

    improved mod08/ex01 span by adding std::sort to shortestSpan calculation

--- module08/ex01/src/Span/Span.cpp
+++ module08/ex01/src/Span/Span.cpp
@@ -33,22 +33,24 @@ void Span::addNumber(int number)
 
 unsigned int Span::shortestSpan() const 
 {
-	if (m_data.size() < 2)
+    if (m_data.size() < 2)
 	{
-		throw std::runtime_error("No span can be found!!!");
-	}
+        throw std::runtime_error("No span can be found!!!");
+    }
 
-	unsigned int shortestSpan = 0;
-    for (size_t i = 0; i < m_data.size(); i++)
+    std::vector<int> sortedData = m_data;
+    std::sort(sortedData.begin(), sortedData.end());
+	unsigned int shortestSpan = std::abs(sortedData[1] - sortedData[0]);
+	
+    for (size_t i = 1; i < sortedData.size(); i++)
 	{
-        for (size_t j = i + 1; j < m_data.size(); j++)
-		{
-            unsigned int span = std::abs(m_data[i] - m_data[j]);
-            if (!shortestSpan || span < shortestSpan)
-                shortestSpan = span;
+        unsigned int span = std::abs(sortedData[i] - sortedData[i - 1]);
+        if (span < shortestSpan)
+		{ 
+            shortestSpan = span;
         }
     }
-	return shortestSpan;
+    return shortestSpan;
 }
 unsigned int Span::longestSpan() const 
 {
@@ -60,8 +62,6 @@ unsigned int Span::longestSpan() const
 	std::vector<int>::const_iterator maxIt = std::max_element(m_data.begin(), m_data.end());
 	return *maxIt - *minIt;
 }
-
-#include <iostream>
 
 void Span::fillSpan(std::vector<int>::iterator itBegin, std::vector<int>::iterator itEnd)
 {
